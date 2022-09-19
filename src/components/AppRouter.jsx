3cec220bbe8eb45f500be040294a8135b2d67f5d
Valiko <skdkls@mail.ru>
2022-09-19 09:10:34 +0400

    MyCommit

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Route, Routes, Redirect } from "react-router-dom";
 import About from "../pages/About";
+import PostedPage from "../pages/PostedPage";
 import Posts from "../pages/Posts";
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route element={<About />} path="/about" />
-      <Route element={<Posts />} path="/posts" />
+      <Route exact element={<Posts />} path="/posts" />
+      <Route exact element={<PostedPage />} path="/posts/:id" />
       <Route path="*" element={<Posts />} />
     </Routes>
   );
